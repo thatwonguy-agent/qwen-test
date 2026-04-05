bdabf43c2c8c5a7611fb1580668de83dc890ce9a
omaragentic <273028384+omaragentic@users.noreply.github.com>
2026-04-04 18:37:48 -0700

    feat: Add ESLint config for React frontend\n\n- Fixed linting errors by adding proper ESLint configuration\n- Includes React, React Hooks, and React Refresh plugins

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-refresh', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'no-console': 'warn',
+  },
+}
